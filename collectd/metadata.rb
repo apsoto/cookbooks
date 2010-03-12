52185893d0a7e14e9bfbec96373a8b9ec62aedbc
Alex Soto <apsoto@gmail.com>
2010-03-12 18:48:44 -0500

    updated collectd maintainer info

--- collectd/metadata.rb
+++ collectd/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "Channels.com"
-maintainer_email "channels-operator@tacitknowledge.com"
+maintainer       "Alex Soto"
+maintainer_email "apsoto AT gmail DOT com"
 license          "Apache 2.0"
 description      "Installs/Configures collectd"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
